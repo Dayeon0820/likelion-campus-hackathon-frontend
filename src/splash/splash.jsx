789928bf2a30f6de7c07059a59ced9f140fd1f55
dani0910 <dlekdms0910@naver.com>
2024-09-20 14:15:57 +0900

    로고 돌아가는 애니메이션 이후에 흰색 로고로 변환이 안됨

--- src/splash/splash.jsx
+++ src/splash/splash.jsx
@@ -1,19 +1,77 @@
 import { React, useState, useEffect } from "react";
-import { Link, useParams, useNavigate } from "react-router-dom";
 import "./splash.css";
 
 const Splash = () => {
+    const [showWhiteLogo, setShowWhiteLogo] = useState(false);
+    const [animationFinished, setAnimationFinished] = useState(false);
 
-    return(
+    useEffect(() => {
+        // 애니메이션이 끝났는지 확인
+        if (animationFinished) {
+            const timer = setTimeout(() => {
+                setShowWhiteLogo(true);
+            }, 3000); // 3초 후에 화이트 로고로 전환
+
+            return () => clearTimeout(timer); // 컴포넌트가 언마운트 될 때 타이머 클리어
+        }
+    }, [animationFinished]);
+
+    const handleAnimationEnd = () => {
+        setAnimationFinished(true); // 애니메이션이 끝났다고 설정
+    };
+
+    return (
         <div id="mobile-view" className="splashBg">
-            {/* <div className="splashLogo"></div> */}
-            {/* <div className="splashLogoSmall"></div> */}
-            <div className="splashBox">
-                <div className="splashBoxLogo"></div>
-                <h1>MOMENT<br/>CLASS</h1>
-            </div>
+            {!showWhiteLogo ? (
+                <div className="splashLogoSmall" onAnimationEnd={handleAnimationEnd}></div>
+            ) : (
+                <div className="splashLogoWhite"></div>
+            )}
         </div>
-    )
+    );
 }
 
 export default Splash;
+
+
+
+
+
+// import { React, useState, useEffect, useRef } from "react";
+// import { Link, useParams, useNavigate } from "react-router-dom";
+// import "./splash.css";
+
+// const Splash = () => {
+
+//     return(
+//         <div id="mobile-view" className="splashBg">
+//             <div
+//                 className= "splashLogoSmall"></div>
+//             <div className="splashLogoWhite"></div>
+//         </div>
+//     );
+// }
+
+// export default Splash;
+
+
+// import { React, useState, useEffect, useRef } from "react";
+// import { Link, useParams, useNavigate } from "react-router-dom";
+// import "./splash.css";
+
+// const Splash = () => {
+
+//     return(
+//         <div id="mobile-view" className="splashBg">
+//             <div
+//                 className= "splashLogoSmall"></div>
+//             <div className="splashBoxLogo"></div>
+//             {/* <div className="splashBox">
+//                 <div className="splashBoxLogo"></div>
+//                 <h1>MOMENT<br/>CLASS</h1>
+//             </div> */}
+//         </div>
+//     );
+// }
+
+// export default Splash;
